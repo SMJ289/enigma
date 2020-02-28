76ef0dcd08128d176019f69d93b31cfe0240e998
Steven Meyers <sjjmeyers@gmail.com>
2020-02-27 22:59:06 -0700

    Add sandbox to gitignore file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,14 @@ require 'date'
 
 class Enigma
 
+  def encrypt(string, key, date)
+    encryption = {}
+  end
+
+  # def encrypt(string, key, date)
+    # encryption_hash = {}
+    # encryption_hash[:encryption] = method_that_encrypts
+    # encryption_hash[:key] = method_that_makes_key
+    # encryption_hash[:date] = method_that_gets_date
+  # end
 end
