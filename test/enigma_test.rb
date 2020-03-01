8d90d8963f8bd260a657a38f245bdd8b61df36d8
Steven Meyers <sjjmeyers@gmail.com>
2020-02-29 19:10:25 -0700

    Add additonal assertion to encrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -134,7 +134,6 @@ class EnigmaTest < Minitest::Test
     }
     @enigma.stubs(:shifts).returns(expected_shifts)
 
-
     expected = {
       encryption: "keder ohulw",
       key: "02715",
@@ -142,6 +141,8 @@ class EnigmaTest < Minitest::Test
     }
 
    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+   @enigma.stubs(:generate_date).returns("040895")
+   assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
 
   def test_it_can_decrypt
@@ -153,7 +154,6 @@ class EnigmaTest < Minitest::Test
     }
     @enigma.stubs(:shifts).returns(expected_shifts)
 
-
     expected = {
       decryption: "hello world",
       key: "02715",
@@ -163,47 +163,4 @@ class EnigmaTest < Minitest::Test
    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
-  def test_it_can_encrypt
-    skip
-    expected = {
-     encryption: "keder ohulw",
-     key: "02715",
-     date: "040895"
-    }
-
-    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-  end
 end
-
-# # decrypt a message with a key and date
-# pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
-# #=>
-# #   {
-# #     decryption: "hello world",
-# #     key: "02715",
-# #     date: "040895"
-# #   }
-#
-# # encrypt a message with a key (uses today's date)
-# pry(main)> encrypted = enigma.encrypt("hello world", "02715")
-# #=> # encryption hash here
-#
-# #decrypt a message with a key (uses today's date)
-# pry(main) > enigma.decrypt(encrypted[:encryption], "02715")
-# #=> # decryption hash here
-#
-# # encrypt a message (generates random key and uses today's date)
-# pry(main)> enigma.encrypt("hello world")
-# #=> # encryption hash here
