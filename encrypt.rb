d8fd71c09883634bef472ce44a42bbb1c6c9938e
Steven Meyers <sjjmeyers@gmail.com>
2020-03-02 15:34:16 -0700

    Add command line interaction to encrypt.rb

--- encrypt.rb
+++ encrypt.rb
@@ -1,3 +1,13 @@
 require './lib/enigma'
 enigma = Enigma.new
-require 'pry'; binding.pry
+
+handle = File.open(ARGV[0], "r")
+
+message = handle.read.chomp
+
+encrypted_message = enigma.encrypt(message)
+
+File.write(ARGV[1], encrypted_message)
+
+print "Created 'encrypted.txt' with the key #{encrypted_message[:key]}
+and date #{encrypted_message[:date]}"
